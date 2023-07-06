7cbf6934c35825eedb2ee1893f1b2798e306534c
Vinicius Ferrarini <viniciusferrarini@outlook.com>
2023-07-06 20:51:33 -0300

    first commit

---
+++ src/main/java/br/com/vncsferrarini/aws_project_one/AwsProjectOneApplication.java
@@ -0,0 +1,13 @@
+package br.com.vncsferrarini.aws_project_one;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AwsProjectOneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AwsProjectOneApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/vncsferrarini/aws_project_one/AwsProjectOneApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.vncsferrarini.aws_project_one;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AwsProjectOneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
